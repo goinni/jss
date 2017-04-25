b5b59dba46a3e3044f1ae7439823617c11b197dc
jerry.hou <goinni@163.com>
2017-04-25 11:47:54 +0800

    update

--- app/src/fn.select.js
+++ app/src/fn.select.js
@@ -33,10 +33,11 @@ _jss.fn.select = function (entity, opt) {
     _this.addStyleSheet("jss-select-panel-3", {
         key: ".jss-select-panel .jss-select-tit",
         value: {
-            'height':height+'px',
+          //  'height':height+'px',
             'line-height': height+'px',
             'border': '1px solid #d2d6de',
-            'width': '100%'
+            'width': '100%',
+            'background-color': 'white'
         }
     });
     _this.addStyleSheet("jss-select-panel-4", {
@@ -140,56 +141,116 @@ _jss.fn.select = function (entity, opt) {
             'margin-top': '-5px'
         }
     });
+    _this.addStyleSheet("jss-select-panel-16", {
+        key: ".jss-select-panel .jss-select-tit .js-mut-t",
+        value: {
+            'padding':'0px 5px',
+            'line-height': (height-6)+'px',
+            'display': 'inline-block',
+            'border': '1px solid #e0e0e0',
+            'margin-left': '5px'
+        }
+    });
+    _this.addStyleSheet("jss-select-panel-17", {
+        key: ".jss-select-panel .jss-select-tit .js-mut-t:hover",
+        value: {
+            'background':'#e0e0e0'
+        }
+    });
 
     select.init = function (opt) {
         var _t = select;
         var data = opt.options;
-        _t.current = data[0], _t.opt = opt, _t.objData = {};
+        _t.current = data[0], _t.opt = opt, _t.objData = {},_t.currentArr=[];
         _this.html(entity, innerContent);
+        if(_t.opt.isMult){
+            jss(entity).find(".jss-select-text").css({"padding":"0 0 0 1px"});
+            _t.needScroll = false;
+        }else{
+            _t.needScroll = true;
+        }
         //jss(entity).find(".jss-select-text").html(_t.current.text);
+        if(opt.ZIndex){
+            jss(entity).find(".jss-select-panel").css({'z-index':opt.ZIndex});
+        }
         jss(entity).find("img").getDom().src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAUBAMAAAB2TKBEAAAAHlBMVEUAAACIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIhgro/iAAAACXRSTlMA5sZlGps6mjl8aeBHAAAAVUlEQVQI12OInAkGUxmYIAwFBhZJED3RgYEhE8SYxsDAwAxiGDAAQSVIKQgwgZQCAVA5UCkYdAKVggGzATojcwYDXDGKdpD90xkQVsAthTsD7jC4UwFVBC5942sWpwAAAABJRU5ErkJggg==";
         if(data.length>7){
             jss(entity).find(".jss-select-content").css({height:"220px","overflow-y":"auto"});
         }
         var selDefault = opt.defaultValue||data[0].value;
+        if(!(selDefault instanceof Array)){
+            selDefault = [selDefault]
+        }
+
+        var defauStr = "HM"+selDefault.join("HM")+"HM";
         var ul = jss(entity).find(".js-select-data-ul");
         var currentLi;
         for (var i = 0; i < data.length; i++) {
             var li = jss("<li>");
-            if(selDefault==data[i].value){
+            if(defauStr.indexOf("HM"+data[i].value+"HM")!=-1){
                 li.addClass("active");
                 _t.current = data[i];
-                jss(entity).find(".jss-select-text").html(_t.current.text);
+                if(_t.opt.isMult){
+                    var spn = jss("<span>");
+                    spn.getDom().id = "hm_mult_t_"+data[i].value;
+                    spn.html(data[i].text+"<i></i>");
+                    spn.attr({"pval":data[i].value});
+                    spn.addClass("js-mut-t");
+                    jss(entity).find(".jss-select-tit").append(spn);
+                    _t.currentArr.push(data[i]);
+                }else{
+                    jss(entity).find(".jss-select-text").html(_t.current.text);
+                }
                 currentLi = li;
             }
-            li.attr({sIndex: i, sval: data[i].value});
+            li.attr({sIndex: i, sval: data[i].value,stext:data[i].text});
             li.html(data[i].text);
             ul.append(li);
             _t.objData[data[i].value] = data[i];
         }
-        _t.needScroll = true;
+
         var liList = jss(entity).find(".jss-select-content").find("li");
         liList.bind("onclick", function (e) {
             jss.stopPropagation(e);
             var item = this;
-            liList.removeClass("active");
-            jss(item).addClass("active");
-            jss(entity).find(".jss-select-panel").removeClass("jss-cont-show");
-            //jss(entity).find("img").removeClass("jss-tab-show");
+            if(_t.opt.isMult){
+                if(jss(item).hasClass("active")){
+                    jss(item).removeClass("active");
+                    jss("#hm_mult_t_"+jss(item).attr("sval")).remove();
+                }else{
+                    jss(item).addClass("active");
+                    var spn = jss("<span>");
+                    spn.getDom().id = "hm_mult_t_"+jss(item).attr("sval");
+                    spn.attr({"pval":jss(item).attr("sval")});
+                    spn.html(jss(item).attr("stext")+"<i></i>");
+                    spn.addClass("js-mut-t");
+                    jss(entity).find(".jss-select-tit").append(spn);
+                    var tit =  jss(entity).find(".jss-select-tit").getDom();
+                    jss(entity).find(".jss-select-content").css({top:(tit.clientHeight+1)+"px"});
+                    spanTitAction();
+                }
+                setCurrArr();
+            }else{
+                liList.removeClass("active");
+                jss(item).addClass("active");
+                jss(entity).find(".jss-select-panel").removeClass("jss-cont-show");
+                jss(entity).find(".jss-select-text").html(_t.current.text);
+            }
+
             _t.current = data[jss(item).attr("sIndex")];
-            jss(entity).find(".jss-select-text").html(_t.current.text);
+
             opt.calback && opt.calback.call(this, _t.current);
 
         });
         var tit = jss(entity).find(".jss-select-tit");
         tit.bind("onclick", function (e) {
             jss.stopPropagation(e);
-            //jss(entity).find(".jss-select-panel").toggleClass("jss-cont-show");
-            //jss(entity).find("img").toggleClass("jss-tab-show");
             var thisTit = jss(entity).find(".jss-select-panel");
             if(thisTit.hasClass("jss-cont-show")){
                 jss(".jss-select-panel").removeClass("jss-cont-show");
             }else{
+                var tit =  jss(entity).find(".jss-select-tit").getDom();
+                jss(entity).find(".jss-select-content").css({top:(tit.clientHeight+1)+"px"});
                 jss(".jss-select-panel").removeClass("jss-cont-show");
                 jss(entity).find(".jss-select-panel").addClass("jss-cont-show");
                 if(_t.needScroll){
@@ -200,6 +261,31 @@ _jss.fn.select = function (entity, opt) {
             }
 
         });
+        spanTitAction();
+        function spanTitAction(){
+            var titSpan = jss(entity).find(".jss-select-tit").find(".js-mut-t");
+            titSpan.unbind("onclick");
+            titSpan.bind("onclick", function (e) {
+                jss.stopPropagation(e);
+                var pval = jss(this).attr("pval");
+                jss(this).remove();
+                var uli = jss(entity).findByAttr("li[sval=" + pval + "]");
+                uli.removeClass("active");
+                setCurrArr();
+            });
+        }
+
+        function setCurrArr(){
+            var titSpans = jss(entity).find(".jss-select-tit").find(".js-mut-t").getDom();
+            if(titSpans){
+                _t.currentArr = [];
+                for(var i=0;i<titSpans.length;i++){
+                    var pVal = jss(titSpans[i]).attr("pval");
+                    _t.currentArr.push(_t.objData[pVal]);
+                }
+            }
+        }
+
         jss(document).bind("onclick", function () {
             jss(entity).find(".jss-select-panel").removeClass("jss-cont-show");
             //jss(entity).find("img").removeClass("jss-tab-show");
@@ -221,6 +307,9 @@ _jss.fn.select = function (entity, opt) {
     };
     select.setValue = function (value) {
         var _t = select;
+        if(_t.opt.isMult){
+            return;
+        }
         var li = jss(entity).findByAttr("li[sval=" + value + "]");
         li.getDom().click();
         _t.current = _t.objData[value];
@@ -228,7 +317,12 @@ _jss.fn.select = function (entity, opt) {
     };
     select.getValue = function () {
         var _t = select;
-        return _t.current;
+        if(_t.opt.isMult){
+            return _t.currentArr;
+        }else{
+            return _t.current;
+        }
+
     };
 
     function scrollLi(val){
