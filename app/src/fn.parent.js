1ba4f7626606720547711048a3383beec41b070d
jerry.hou <goinni@163.com>
2017-07-24 21:15:50 +0800

    update

--- app/src/fn.parent.js
+++ app/src/fn.parent.js
@@ -2,10 +2,16 @@
  * 获取元素的父节点
  */
 _jss.fn.parent = function(result){
-	var r = result.parentNode;
-	while(!r.tagName) {
-		r = r.parentNode;
-		if(!r)return;
+	var r = null;
+	if(result){
+		try{
+			r = result.parentNode;
+			while(!r.tagName) {
+				r = r.parentNode;
+				if(!r)return;
+			}
+		}catch(e){}
 	}
+	
 	return r;
 }
