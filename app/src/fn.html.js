b6a0d282043b2c4de3956cde9225e6a572f30743
gaoxuan <gaoxuan@huimei.com>
2018-05-02 17:34:49 +0800

    jss

--- app/src/fn.html.js
+++ app/src/fn.html.js
@@ -11,9 +11,21 @@ _jss.fn.html = function(arr, text){
 		var entity = arr[0];
 		return entity.innerHTML;
 	}
-	// 设置
-	for(var i = 0; i<arr.length; i++){
-	    var entity = arr[i];
-	    entity.innerHTML = text || "";
+	if(text.indexOf('<style>')!=-1 || text.indexOf('</style>')!=-1){
+		var temp = this.createElementByTagName();
+		temp.innerHTML = "x<div>"+innerhtml+"</div>";
+		// 设置
+		for(var i = 0; i<arr.length; i++){
+			var cloneNode = temp.lastChild.cloneNode(true);
+			var entity = arr[i];
+			entity.appendChild(cloneNode);
+		}
+	}else{
+		// 设置
+		for(var i = 0; i<arr.length; i++){
+			var entity = arr[i];
+			entity.innerHTML = text || "";
+		}
 	}
+
 }
