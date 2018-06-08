a3ed17a8b2c24a2b90d0d30b634601fa7af82828
gaoxuan <gaoxuan@huimei.com>
2018-06-08 14:24:40 +0800

    jss

--- app/src/fn.buried.js
+++ app/src/fn.buried.js
@@ -4,24 +4,12 @@
  */
 _jss.fn.addBuried = function(src,flag) {
 	var img = document.getElementById("hmCdssBuriedImg");
-	var evt = document.getElementById("hmCdssEventImg");
-	var imgLoad = true;
 	if(!img){
 		img = document.createElement("img");
 		img.setAttribute("id", "hmCdssBuriedImg");
 		document.body.appendChild(img);
 	}
-	if(!evt){
-		evt = document.createElement("img");
-		evt.setAttribute("id", "hmCdssEventImg");
-		document.body.appendChild(evt);
-	}
-	if (flag) {
-		var cloneEvt = evt.cloneNode(true);
-		cloneEvt.src = src;
-	} else {
-		var cloneEvt = img.cloneNode(true);
-		cloneEvt.src = src;
-	}
+	var cloneEvt = img.cloneNode(true);
+	cloneEvt.src = src;
 
 }
